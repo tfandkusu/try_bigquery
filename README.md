272d5b7d60e1e4583d1b80e07cec4045471f1e7e
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-08-14 00:28:27 +0900

    月別集計完了

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # try_bigquery
 
 PythonでのBigQueryからのデータの取り出しとBigQueryテーブルへの追記のやり方を確認する
+
+# セットアップ方法
+
+M1 Macの場合
+
+```sh
+pyenv install
+pip install poetry
+export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
+poetry install
+```
--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-print("Hello World")
+from google.cloud import bigquery
 
 # シカゴ市の人口2000, 2010, 2020
 POPULATIONS = [2896016, 2695598, 2746388]
+
+client = bigquery.Client()
+
+query = """
+SELECT
+FORMAT('%04d/%02d', EXTRACT(year FROM date),EXTRACT(month FROM date)) as month,
+COUNT(unique_key) AS count
+FROM `bigquery-public-data.chicago_crime.crime`
+GROUP BY month
+ORDER BY month ASC
+"""
+table = client.get_table("tfandkusu.try_bq.crime_month")
+query_job = client.query(query)
+
+for row in query_job:
+    month = row["month"]
+    count = row["count"]
+    population = 0
+    if month < "2010":
+        population = POPULATIONS[0]
+    elif month < "2020":
+        population = POPULATIONS[1]
+    else:
+        population = POPULATIONS[2]
+    value = 100000.0 * count / population
+    json_rows = [{"month": month, "value": value}]
+    errors = client.insert_rows_json(table, json_rows)
+    if errors != []:
+        print(errors)
